3911900d7a2ad5ef683dee6f24f1beb4b4b32bc8
Gabriel Ritter <117668639+gabrielritter99@users.noreply.github.com>
2022-11-15 01:34:52 -0300

    AppError por último

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,8 @@ const app = express();
 
 app.use(express.json());
 
+app.use(routes);
+
 app.use((error: Error, request: Request, response: Response, next:NextFunction)=> {
     if(error instanceof AppError){
         return response.status(error.statusCode).json({
@@ -23,7 +25,5 @@ app.use((error: Error, request: Request, response: Response, next:NextFunction)=
         message: 'internal server error'
     })
 });
-
-app.use(routes);
 
 app.listen(3000, ()=> console.log("Server is running!!!"))
